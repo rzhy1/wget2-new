40f3c5be0684259472247cb58bf3592321632848
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-10-23 21:42:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -195,7 +195,32 @@ build_wget2() {
   LIBPSL_LIBS="-L$INSTALLDIR/lib -lpsl" \
   LIBPCRE2_CFLAGS=$CFLAGS \
   LIBPCRE2_LIBS="-L$INSTALLDIR/lib -lpcre2-8"  \
-  ./configure --build=x86_64-pc-linux-gnu --host=$PREFIX --with-libiconv-prefix="$INSTALLDIR" --with-ssl=gnutls --disable-shared --enable-static --without-lzma  --with-zstd --without-bzip2 --without-lzip --without-gpgme --enable-threads=windows || exit 1
+  ./configure --build=x86_64-pc-linux-gnu --host=$PREFIX --with-libiconv-prefix="$INSTALLDIR" \
+    --with-ssl=gnutls \
+    --disable-shared \
+    --enable-static \
+    --disable-nls \
+    --disable-doc \
+    --disable-code-coverage \
+    --disable-manywarnings \
+    --disable-fuzzing \
+    --disable-fsanitize-ubsan \
+    --disable-fsanitize-asan \
+    --disable-fsanitize-msan \
+    --disable-fsanitize-tsan \
+    --disable-assert \
+    --disable-valgrind-tests \
+    --enable-year2038 \
+    --with-libhsts \
+    --with-libnghttp2 \
+    --with-gpgme \
+    --with-zlib \
+    --with-lzip \
+    --with-libidn2 \
+    --without-lzma  \
+    --with-zstd \
+    --without-bzip2 \
+    --enable-threads=windows || exit 1
   make -j$(nproc)  || exit 1
   strip $INSTALLDIR/wget2/src/wget2.exe || exit 1
   cp -fv "$INSTALLDIR/wget2/src/wget2.exe" "${GITHUB_WORKSPACE}" || exit 1
