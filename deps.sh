07f46fce69745128fbc18c35ce1d61329193a3ee
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2026-02-07 00:52:18 +0800

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -76,9 +76,9 @@ build_libunistring() {
   echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build libunistring⭐⭐⭐⭐⭐⭐" 
   wget -O- https://mirrors.kernel.org/gnu/libunistring/libunistring-1.4.1.tar.gz | tar xz || exit 1
   cd libunistring-* || exit 1
-  ./configure CFLAGS="-Os" --build=x86_64-pc-linux-gnu --host=$PREFIX --prefix=$INSTALLDIR --disable-shared --enable-static --disable-silent-rules --enable-threads=windows || exit 1
-  make -j$(nproc) all-lib || exit 1
-  make install || exit 1
+  ac_cv_func_nanosleep=yes ./configure CFLAGS="-Os" --build=x86_64-pc-linux-gnu --host=$PREFIX --prefix=$INSTALLDIR --disable-shared --enable-static --disable-silent-rules --enable-threads=windows || exit 1
+  make -C src -j$(nproc) || exit 1
+  make -C src install || exit 1
   cd .. && rm -rf libunistring-*
 }
 
