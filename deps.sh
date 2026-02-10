0f571339929ab349bcf0329cca51c5a956780ff4
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2026-02-10 19:44:33 +0800

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -191,7 +191,7 @@ build_gnutls() {
   #ifdef __cplusplus\n\
   }\n\
   #endif' lib/includes/gnutls/socket.h
-  sed -i 's|#include <minmax.h>\n#include <byteswap.h>|#include <minmax.h>\n#ifdef _WIN32\n#include <stdlib.h>\n#define bswap_16(x) _byteswap_ushort(x)\n#define bswap_32(x) _byteswap_ulong(x)\n#define bswap_64(x) _byteswap_uint64(x)\n#else\n#include <byteswap.h>\n#endif|' lib/num.h
+  sed -i 's|#include <byteswap.h>|#ifdef _WIN32\n#include <stdlib.h>\n#define bswap_16(x) _byteswap_ushort(x)\n#define bswap_32(x) _byteswap_ulong(x)\n#define bswap_64(x) _byteswap_uint64(x)\n#else\n#include <byteswap.h>\n#endif|' lib/num.h
 
   export gl_cv_func_nanosleep=yes
   export gl_cv_func_clock_gettime=yes
