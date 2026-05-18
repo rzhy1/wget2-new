510580ff873b8fb191c3ab356f7de3361abcb57c
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2026-05-18 09:18:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -153,10 +153,12 @@ build_libpsl() {
 
 build_wget2() {
   echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build wget2⭐⭐⭐⭐⭐⭐" 
-  git clone --recursive --depth=1 https://gitlab.com/gnuwget/wget2.git || exit 1
+  git clone --depth=1 https://gitlab.com/gnuwget/wget2.git || exit 1
   cd wget2 || exit 1
+  rm -rf gnulib
+  git clone --depth=1 https://github.com/coreutils/gnulib.git
   sed -i '/include gnulib.mk/i MAINTAINERCLEANFILES =' lib/Makefile.am
-  AUTOPOINT=true ./bootstrap || exit 1
+  AUTOPOINT=true ./bootstrap --skip-po --gnulib-srcdir=gnulib || exit 1
 
   # ========== 应用源码补丁，修复已知警告 ==========  
   # 1. blacklist.c: 修复返回局部变量地址（第156行）
